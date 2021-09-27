c52e44bd890173d787c27a9902766ae30231d1cd
poseidon-code <pritamhalder05062000@gmail.com>
2021-09-27 23:23:39 +0530

    add: stack-03

---
+++ stack/03-next-greater.go
@@ -0,0 +1,37 @@
+// PROBLEM: Next greater element
+// https://leetcode.com/problems/next-greater-element-ii/
+// https://practice.geeksforgeeks.org/problems/next-larger-element/0
+
+package main
+
+import (
+	"fmt"
+)
+
+// SOLUTION
+func next_greater(nums []int) []int {
+	n := len(nums)
+	var stack []int
+	result := make([]int, n)
+	for i:=0; i<n; i++ {
+		result[i] = -1
+	}
+
+	for i:=0; i<n*2; i++ {
+		for len(stack)!=0 && (nums[stack[len(stack)-1]] < nums[i%n]) {
+			result[stack[len(stack)-1]] = nums[i%n]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i%n)
+	}
+	return result
+}
+
+
+func main() {
+	// INPUT :
+	nums := []int{1,2,3,4,3}
+
+	// OUTPUT :
+	fmt.Println(next_greater(nums))
+}
